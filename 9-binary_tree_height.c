d827663bf402362579ec69a37384f3d519324c0b
root <root@jonathan-pc.localdomain>
2023-05-30 23:55:47 +0300

    corrections 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,29 +8,18 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int counter = 0, left = 0, right = 0;
-
-	if (tree == NULL)
+	if (tree)
 	{
-		return (0);
+		int left = 0, right = 0;
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
 	}
 	else
-	{
-		if (tree->right != NULL)
-		{
-			right = 1 + binary_tree_height(tree->right);
-		}
-		if (tree->left != NULL)
-		{
-			left = 1 + binary_tree_height(tree->left);
-		}
-
-		if (right > left)
-		{
-			return (right);
-		}
-		else
-			return (left);
-
-	}
+		return (0);
 }
