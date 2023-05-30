8762c54a8319a688f0232ce84e35b034c379c389
Jany Muong <me.roumuong@gmail.com>
2023-05-30 17:14:02 +0300

    insert node as left-child of a node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,7 +6,7 @@
  * @value: data value to store in the new node
  *
  * Return: pointer to the created node,
- * 		or NULL on failure or if parent is NULL
+ *		or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
