9e3fb2a625c8bd76acd4e01ee0c36f2f9f0f62c0
Jany Muong <me.roumuong@gmail.com>
2023-05-31 05:56:35 +0300

    fix betty: trailing whitespace,

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,16 +2,17 @@
 
 /**
 * binary_tree_height - function that checks the height of a binary tree
-* @tree: A pointer to the root node of the tree to measure the height 
+* @tree: A pointer to the root node of the tree to measure the height
 * Return: if tree is NULL, your function must return 0.
 */
 
 size_t binary_tree_height(const binary_tree_t *tree)
-{	
+{
 	/* if tree is NULL return 0 */
 	if (tree)
 	{
 		int left = 0, right = 0;
+
 		if (tree->right)
 			right = 1 + binary_tree_height(tree->right);
 		if (tree->left)
