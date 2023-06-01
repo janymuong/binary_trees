e44dc6719ca678e693d19583a8e0a4747660a75e
Sir Jonathan Kariuki <jonamkariuki@gmail.com>
2023-06-01 17:00:32 +0300

    fixed betty errors 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/*
+/**
 * binary_tree_height - measures the height of a binary tree.
 * @tree: pointer to the root node of the tree to measure the height.
 * Return: if tree is NULL, your function must return 0.
