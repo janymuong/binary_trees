1d6e62503709e79524dbe610c75336013c9d6470
Jany Muong <me.roumuong@gmail.com>
2023-06-01 18:23:06 +0300

    check for lowest common ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,7 +1,10 @@
 #include "binary_trees.h"
 
+int bin_is_descendant(const binary_tree_t *node, const binary_tree_t *target);
+
 /**
  * binary_trees_ancestor - loookup the lowest common ancestor of two nodes
+ *				recursivly
  * @first: pointer to the first node
  * @second: pointer to the second node
  *
@@ -11,20 +14,40 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
+	binary_tree_t *bin_ancestor;
+
 	if (first == NULL || second == NULL)
 		return (NULL);
 
-	if (first == second)
-		return ((binary_tree_t *)first);
+	bin_ancestor = (binary_tree_t *)first;
 
-	if (first == second->parent)
-		return ((binary_tree_t *)first);
+	while (bin_ancestor != NULL)
+	{
+		if (bin_is_descendant(bin_ancestor, second))
+			return (bin_ancestor);
+		bin_ancestor = bin_ancestor->parent;
+	}
 
-	if (first->parent == second)
-		return ((binary_tree_t *)second);
+	return (NULL);
+}
 
-	if (first->parent == second->parent)
-		return ((binary_tree_t *)first->parent);
 
-	return (binary_trees_ancestor(first->parent, second->parent));
+/**
+ * bin_is_descendant - checks for descendant of another node
+ *			recursion
+ * @node: pointer to the potential descendant node
+ * @target: pointer to the potential ancestor node
+ *
+ * Return: 1 if @node is a descendant of @target, else 0
+ */
+int bin_is_descendant(const binary_tree_t *node, const binary_tree_t *target)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node == target)
+		return (1);
+
+	return (bin_is_descendant(node->left, target) ||
+			bin_is_descendant(node->right, target));
 }
