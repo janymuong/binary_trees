9f5576c1fe287270200a29e7a0302793a4023434
Sir Jonathan Kariuki <jonamkariuki@gmail.com>
2023-06-01 16:54:32 +0300

    fixed betty errors 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -25,6 +25,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	else
-	return (binary_tree_is_leaf(tree) + binary_tree_leaves(tree->right) + binary_tree_leaves(tree->left));
+	return (binary_tree_is_leaf(tree) + binary_tree_leaves(tree->right) + 
+			binary_tree_leaves(tree->left));
 }
